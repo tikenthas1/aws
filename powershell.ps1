b0b83781cdd1a22fb839b94ae0110f8b2439b920
tikenthas1 <kaple1912@gmail.com>
2025-05-11 18:06:28 +0530

    Chnges

--- powershell.ps1
+++ powershell.ps1
@@ -1,12 +1,14 @@
-$filepath = D:\Powershell-LAB\abc.txt
+D:\Powershell-LAB\abc.txt
 try {
-    if(Test-Path $filepath)
+    if(Test-Path $filepath){
     $content = Get-Content $filepath
     Write-Host "file content:"
     Write-Host $content
 }
 
-    else(Write-Host "File does not foundat $filepath")
+    else{
+    (Write-Host "File does not foundat $filepath")
+    }
 }
 catch {
     Write-Host "An error occured while trying to read file " -ForegroundColor Red
